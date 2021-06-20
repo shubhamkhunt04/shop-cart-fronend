d80a1cd97ddb09c7e8d5febcf8e5c7b2a50a9170
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-06-20 09:28:52 +0530

    retrive product details by id

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,6 +4,7 @@ const { validateProductInput } = require('../util/validators/productValidator');
 
 const productRouter = express.Router();
 
+// products route
 productRouter
   .get('/', async (req, res) => {
     try {
@@ -75,27 +76,28 @@ productRouter
     }
   });
 
-// productRouter
-//   .get('/:userId/posts', async (req, res) => {
-//     result = {};
-//     try {
-//       const user = await User.findById(req.params.userId).populate('posts');
-//       res.send(user.posts);
-//     } catch (error) {
-//       throw new Error('Post not Found');
-//     }
-//   })
-//   .post('/:userId/createpost', async (req, res) => {
-//     result = {};
-//     try {
-//       const post = await Post.create(req.body);
-//       const user = await User.findById(req.params.userId);
-//       user.posts.push(post.id);
-//       await user.save();
-//       res.send(post);
-//     } catch (error) {
-//       throw new Error('Post not create');
-//     }
-//   });
+// single product route
+productRouter.get('/:productId', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+      return res.json({
+        message: 'Product not found',
+        status: 200,
+      });
+    }
+    return res.json({
+      payload: product,
+      message: 'Product fetched successfully',
+      status: 200,
+    });
+  } catch (error) {
+    return res.json({
+      message: 'Something went wrong !',
+      err: error.message,
+      status: 400,
+    });
+  }
+});
 
 module.exports = productRouter;
