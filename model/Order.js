2c90bfd1d743b14066f07df107c51eb7102a7772
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-06-20 22:52:01 +0530

    remove server code

--- model/Order.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const OrderSchema = new Schema({
-  orderedProducts: [],
-});
-
-module.exports = mongoose.model('Order', OrderSchema);
